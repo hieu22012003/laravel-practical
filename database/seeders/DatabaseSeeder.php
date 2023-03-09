4bc2bf42cf48da6f14159b9e31a772b346fdea0e
hieuelcom <112467458+hieuelcom@users.noreply.github.com>
2023-03-09 10:11:00 +0700

    push

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,14 +9,55 @@ class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('_book')->insert([
+            [
+                'id' => 1,
+                'bookid' => 101,
+                'authorid'=> 11,
+                'title' => 'Lean PHP',
+                'ISBN' => ' A06162',
+                'pub_year'=> 2019,
+                'available'=> 90,
+            ],
+            [
+                'id' => 2,
+                'bookid' => 102,
+                'authorid'=> 99,
+                'title' => 'Lean HTML',
+                'ISBN' => ' A06163',
+                'pub_year'=> 2022,
+                'available'=> 60,
+            ],
+            [   'id' => 3,
+                'bookid' => 103,
+                'authorid'=> 30,
+                'title' => 'Lean Laravel',
+                'ISBN' => ' A06164',
+                'pub_year'=> 2020,
+                'available'=> 10,
+            ],
+            [   'id' => 4,
+                'bookid' => 104,
+                'authorid'=> 50,
+                'title' => 'Lean C',
+                'ISBN' => ' A06165',
+                'pub_year'=> 2021,
+                'available'=> 10,
+            ],
+            [   'id' => 5,
+                'bookid' => 105,
+                'authorid'=> 50,
+                'title' => 'Lean Boostrap',
+                'ISBN' => ' A06166',
+                'pub_year'=> 2014,
+                'available'=> 10,
+            ],
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/',[SearchController::class,'search'])->name('web.search');
