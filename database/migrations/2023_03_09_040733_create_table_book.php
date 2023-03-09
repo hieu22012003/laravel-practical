e65f2119165cd27b15097d911aec5ce3f400b37a
hieuelcom <112467458+hieuelcom@users.noreply.github.com>
2023-03-09 11:33:59 +0700

    push

--- database/migrations/2023_03_09_040733_create_table_book.php
+++ database/migrations/2023_03_09_040733_create_table_book.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('table_book', function (Blueprint $table) {
+        Schema::create('book', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('author_id');
             $table->string("title",55);
